8b76652fac803a714687aa91f9f3b5c5f882bb4b
Itachi-MacOS <warrior.006@inbox.ru>
2025-07-19 09:57:03 +0500

    feat(auth): add authentication flow with loading state
    Add authentication component that redirects to home page after successful login. Show loading spinner during auth process.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
 import HomeScreen from './home'
 import IntercomCallScreen from './accept'
 import { QueryClientProvider } from '@tanstack/react-query'
-import queryClient from './queries/client'
+import queryClient, { pb } from './queries/client'
+import { useEffect } from 'react'
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomeScreen />} />
+          <Route path="/" element={<Authenticate />} />
+          <Route path="/home" element={<HomeScreen />} />
           <Route path="/accept" element={<IntercomCallScreen />} />
           {/* Add more routes as needed */}
         </Routes>
@@ -19,3 +21,22 @@ function App() {
 }
 
 export default App
+
+function Authenticate() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const fn = async () => {
+      const auth = await pb
+        .collection('users')
+        .authWithPassword('webrtc_native', '12345678');
+      if (auth) {
+        navigate('/home')
+      }
+    }
+    fn()
+  }, [])
+  return <div className='flex justify-center items-center h-screen'>
+    <div className='w-16 h-16 border-4 border-dashed rounded-full animate-spin'></div>
+  </div>
+}
